5caff02c815ed3d90f1bcea70b7333eb757e8d15
Felipe Amorim <felipe.moreira2601@hotmail.com>
2025-10-07 17:10:50 -0300

    form indo para whats

--- js/app.js
+++ js/app.js
@@ -18,10 +18,8 @@ i18next
       },
     },
     function (err, t) {
-      // Inicializa a tradução quando estiver pronta
       updateContent();
 
-      // Só carrega o project.js aqui, depois que o i18next estiver pronto
       if (window.location.pathname.includes("project.html")) {
         const script = document.createElement("script");
         script.src = "js/project.js";
@@ -86,6 +84,42 @@ window.addEventListener("scroll", function () {
   } else {
     backToTopButton.classList.remove("show");
   }
+});
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const nome = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const telefone = document.querySelector("#phone").value;
+  const empresa = document.querySelector("#company").value;
+  const instagram = document.querySelector("#instagram").value;
+  const comoConheceu = document.querySelector('input[name="conheceu"]:checked')
+    ? document.querySelector('input[name="conheceu"]:checked').value
+    : "";
+  const servicos = Array.from(
+    document.querySelectorAll('input[name="servicos"]:checked')
+  )
+    .map((input) => input.value)
+    .join(", ");
+  const descricao = document.querySelector("#business").value;
+
+  let mensagem = `Vim pelo formulário de contato do site estudioagartha.com\n`;
+  mensagem += `*Nome:* ${nome}\n`;
+  mensagem += `*Email:* ${email}\n`;
+  mensagem += `*Telefone:* ${telefone}\n`;
+  mensagem += `*Nome da empresa:* ${empresa}\n`;
+  mensagem += `*Instagram da empresa:* ${instagram}\n`;
+  mensagem += `*Como nos conheceu:* ${comoConheceu}\n`;
+  mensagem += `*Serviços desejados:* ${servicos}\n`;
+  mensagem += `*Descrição do negócio:* ${descricao}`;
+
+  mensagem = encodeURIComponent(mensagem);
+
+  const numeroWhatsApp = "5574999396200";
+  const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagem}`;
+
+  window.open(urlWhatsApp, "_blank");
 });
 
 document.getElementById("back-to-top").addEventListener("click", function (e) {
