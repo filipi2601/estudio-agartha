6311d3072179a8095ed9e0fd755691ad63e12ca0
Felipe Amorim <felipe.moreira2601@hotmail.com>
2026-08-17 11:39:02 -0300

    finalizando migracao

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://studioagartha.com",
+  integrations: [react(), icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
